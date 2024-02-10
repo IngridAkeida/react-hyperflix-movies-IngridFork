267d781681c024486677cf1a9c246412eb2428aa
IngridAkeida <ingridsanchesp@gmail.com>
2024-02-10 16:49:47 +0100

    reduce - repeating styles

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -28,15 +28,20 @@ const SearchResults = () => {
     return title.length > maxLength ? title.slice(0, maxLength) + '...' : title;
   };
 
+  //style components 
+  const styleUl = 'flex flex-wrap gap-4';
+  const styleLi = 'p-3 hover:bg-m_darkGrey cursor-pointer';
+  const styleFirstDiv = 'w-[80vw] sm:w-[40vw] md:w-[16vw] lg:w-[16vw] h-[220px] inline-block overflow-hidden';
+
   if (loading) {
     return (
       <div className='pt-[120px]'>
         <h2 className='p-4'>Search Results</h2>
         <div className='px-8'>
-          <ul className='flex flex-wrap gap-4'>
+          <ul className={styleUl}>
             {[...Array(15)].map((_, index) => (
-              <li className='p-3 hover:bg-m_darkGrey cursor-pointer' key={index}>
-                <div className='w-[80vw] sm:w-[40vw] md:w-[16vw] lg:w-[16vw] h-[220px] inline-block overflow-hidden'>
+              <li className={styleLi} key={index}>
+                <div className={styleFirstDiv}>
                   <Skeleton className='w-[600px] h-64 rounded-md' />
                 </div>
                 <div className='flex justify-between'>
@@ -55,11 +60,11 @@ const SearchResults = () => {
     <div className='pt-[120px]'>
       <h2 className='p-4'>Search Results</h2>
       <div className='px-8'>
-        <ul className='flex flex-wrap gap-4'>
+        <ul className={styleUl}>
           {resultsQuery.map((movie) => (
-            <li className='p-3 hover:bg-m_darkGrey cursor-pointer' key={movie.id}>
+            <li className={styleLi} key={movie.id}>
               <Link to={`/movie/${movie.id}`}>
-                <div className='w-[80vw] sm:w-[40vw] md:w-[16vw] lg:w-[16vw] h-[220px] inline-block overflow-hidden '>
+                <div className={styleFirstDiv}>
                   <img className='w-full h-full block object-cover' src={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : defaultMovie} alt={movie.title} />
                 </div>
                 <div className='flex justify-between w-full'>
